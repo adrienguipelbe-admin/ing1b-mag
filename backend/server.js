f1873751c60332d7b93f3ebccce5dde17434b8de
Adrien <adrienguipelbe@gmail.com>
2026-04-04 23:53:54 +0100

    Fix CORS production

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ app.use(cors({
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
+    'https://creative-cajeta-bef7ac.netlify.app',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   credentials: true
